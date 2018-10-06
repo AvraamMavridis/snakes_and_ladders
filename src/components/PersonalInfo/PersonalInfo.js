67ca9ac563ca1a558fa823e11a29c7a1c83e1a7c
AvraamMavridis <avraammauridis@gmail.com>
2018-10-06 12:16:06 +0200

    Chore: Add personal link

---
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './PersonalInfo.css';
+
+export default class PersonalInfo extends Component {
+  render() {
+    return (
+      <div className='personal-info'>
+        <a href="https://www.linkedin.com/in/avrmav/">Avraam Mavridis</a>
+      </div>
+    );
+  }
+}
+
