2ce27ed669d46cf07ca6efa23b42526695c38230
AvraamMavridis <avraammauridis@gmail.com>
2018-10-06 08:36:54 +0200

    Updates

---
+++ precache-manifest.908817030ade70ec0314b48efc23d699.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c1ec8d0c3dfc4c2147ec90e6a9a2016",
+    "url": "/snakes_and_ladders/static/media/hero2.3c1ec8d0.png"
+  },
+  {
+    "revision": "e5eac10f5b75401c048dc2456c3ab58c",
+    "url": "/snakes_and_ladders/static/media/hero.e5eac10f.png"
+  },
+  {
+    "revision": "6dd8eb80700e823a0b01b0517de65240",
+    "url": "/snakes_and_ladders/static/media/dice.6dd8eb80.png"
+  },
+  {
+    "revision": "9485ed2bdb309a00bb6be1446479af4f",
+    "url": "/snakes_and_ladders/static/media/board.9485ed2b.jpg"
+  },
+  {
+    "revision": "ab27efa40f3fc349bfe4",
+    "url": "/snakes_and_ladders/static/js/runtime~main.ab27efa4.js"
+  },
+  {
+    "revision": "fd892faaf24f2b4777d8",
+    "url": "/snakes_and_ladders/static/js/main.fd892faa.chunk.js"
+  },
+  {
+    "revision": "cca754d8420e7e6c218f",
+    "url": "/snakes_and_ladders/static/js/1.cca754d8.chunk.js"
+  },
+  {
+    "revision": "fd892faaf24f2b4777d8",
+    "url": "/snakes_and_ladders/static/css/main.90c26409.chunk.css"
+  },
+  {
+    "revision": "d97b1bd58458d2741c3fc8976255ff8c",
+    "url": "/snakes_and_ladders/index.html"
+  }
+];
